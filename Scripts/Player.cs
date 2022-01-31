fc61514675b6f81966b0ccb15b2a04fbdc099428
plyr0 <plyr00@gmail.com>
2022-01-31 21:44:59 +0100

    mouse

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,11 +7,26 @@ public class Player : Area2D
 	[Signal] public delegate void Hit(); 
 
 	private Vector2 screenSize = Vector2.Zero;
+	private bool touchHeld = false;
+	private Vector2 touchPosition = new Vector2();
 
 	public override void _Ready()
 	{
 		screenSize = GetViewportRect().Size;
 		Hide();
+	}
+
+	public override void _Input(InputEvent ev)
+	{
+		if (ev is InputEventMouseMotion motion)
+		{
+			touchHeld = motion.Pressure > 0;
+			touchPosition = motion.Position;
+		}
+		else
+		{
+			base._Input(ev);
+		}
 	}
 
 	public override void _Process(float delta)
@@ -23,6 +38,16 @@ public class Player : Area2D
 		if (Input.IsActionPressed("move_down")) direction.y += 1;
 		if (Input.IsActionPressed("move_left")) direction.x -= 1;
 		if (Input.IsActionPressed("move_right")) direction.x += 1;
+		if (touchHeld)
+		{
+			var x = touchPosition.x - Position.x;
+			if (Mathf.Abs(x) > 27)
+				direction.x = x;
+
+			var y = touchPosition.y - Position.y;
+			if (Mathf.Abs(y) > 27)
+				direction.y = y;
+		}
 
 		if (direction.Length() > 0)
 		{
